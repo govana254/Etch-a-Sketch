b3844d336e809480004dc87ac9730b5f87611d7c
GAD KAMAU <gadkamau46@gmail.com>
2023-08-11 08:19:03 +0300

    fixed some bugs

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const container = document.querySelector(".container");
-const clear = document.querySelector("button");
+let clear = document.querySelector("button");
+let container = document.querySelector(".container"); 
 
-clear.addEventListener('click', ()=> {
+clear.addEventListener('click', ()=>{
     resetSize()
 })
 
+
 function resetSize(){
-    let number = prompt ("What size would you like the grid to be? (1-100)");
-    container.style.gridTemplateRows = `repeat(${number}, 1fr)`
-    container.style.gridTemplateColumns = `repeat(${number}, 1fr)`
-    createGrid(number)
+        
+        let number = prompt("What size would you like the grid to be? (1-100)");
+        container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+        createGrid(number); 
 }
 
-function createGrid(size) {
-    container.style.setTemplateRows =`repeat(${size}),1fr`;
-    container.style.setTemplateColumns =`repeat(${size}),1fr`;
+function createGrid(size){
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     
-    for (let c = 0; c < (size * size); c++) {
+    for (let i = 0; i < size*size; i++) {
         
-        let cell = document.createElement("div");
-        cell.style.backgroundColor = "black"
-        container.appendChild(cell);
-        
-        cell.addEventListener("mouseover", e => {
-            cell.style.backgroundColor = "white";
+        let square = document.createElement("div");
+        square.style.backgroundColor = "black";
+        container.appendChild(square);
+
+        //change background color of a square on hover
+        square.addEventListener('mouseover', e=>{
+            square.style.backgroundColor = "white";
         })
+
+
+        //function to reset the grid
         function clearGrid(){
-                clear.addEventListener('click', e => {square.style.backgroundColor = "black"
+            clear.addEventListener('click', e=>{
+                square.style.backgroundColor = "black"
             })
         }
+        
 
-        clearGrid()
-    };
-};
+        clearGrid();    
+    }
+}
 
 createGrid(16);
